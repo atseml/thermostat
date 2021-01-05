14a027c0f02ef0b44ea78ddaffd22ff79a2a9cc1
topquark12 <aw@m-labs.hk>
2021-01-05 12:42:50 +0800

    pytec: revert changes to example.py and plot.py, save changes for other PR

--- pytec/example.py
+++ pytec/example.py
@@ -1,13 +1,11 @@
-if __name__ == "__main__":
+from pytec.client import Client
 
-    from pytec.client import Client
-
-    tec = Client() #(host="localhost", port=6667)
-    tec.set_param("s-h", 1, "t0", 20)
-    print(tec.get_pwm())
-    print(tec.get_pid())
-    print(tec.get_pwm())
-    print(tec.get_postfilter())
-    print(tec.get_steinhart_hart())
-    for data in tec.report_mode():
-        print(data)
+tec = Client() #(host="localhost", port=6667)
+tec.set_param("s-h", 1, "t0", 20)
+print(tec.get_pwm())
+print(tec.get_pid())
+print(tec.get_pwm())
+print(tec.get_postfilter())
+print(tec.get_steinhart_hart())
+for data in tec.report_mode():
+    print(data)
