6fe2f151ff8925773d3271254bb7f0d5d89fda80
topquark12 <aw@m-labs.hk>
2021-01-06 10:36:43 +0800

    remove blank line

--- pytec/autotune.py
+++ pytec/autotune.py
@@ -245,7 +245,6 @@ def main():
     for data in tec.report_mode():
         try:
             ch = data[channel]
-
         # Sometimes report_mode may yeild empty object
         except KeyError:
             continue
